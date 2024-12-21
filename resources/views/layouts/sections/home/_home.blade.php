efdc7bbb64b8e8ec6e7971d6e51dcd4952bee1f6
Ozlmaltn <ozlmaltn8@gmail.com>
2024-12-21 14:12:45 +0300

    Anasayfa Taslak

--- resources/views/layouts/sections/home/_home.blade.php
+++ resources/views/layouts/sections/home/_home.blade.php
@@ -1,1 +1,22 @@
 <h2 class="text-center mt-5 mb-5">Home Sayfası</h2>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            Chatboot Kısmı
+        </div>
+        <div class="col-md-2">
+            Restorant Menü 1
+        </div>
+        <div class="col-md-2">
+            Restorant Menü 2
+        </div>
+        <div class="col-md-2">
+            Restorant Menü 3
+        </div>
+        <div class="col-md-2">
+            Restorant Menü 4
+        </div>
+    </div>
+</div>
