045c8e299a0ec99f71197556ef3cbdc08043e67b
Bektas Ergin <bektas.ergin04@gmail.com>
2024-12-27 15:03:35 +0300

    login,register,forgotpassword add

--- resources/views/layouts/sections/home/_home.blade.php
+++ resources/views/layouts/sections/home/_home.blade.php
@@ -1,1 +1,12 @@
+@if(session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+@endif
+
+@if(session('error'))
+    <div class="alert alert-danger">
+        {{ session('error') }}
+    </div>
+@endif
 <h2 class="text-center mt-5 mb-5">Home Sayfası</h2>
