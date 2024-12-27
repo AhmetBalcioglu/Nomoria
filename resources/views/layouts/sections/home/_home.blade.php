46ce5e320dda0982233fb949767838f8f7327cd9
Ozlmaltn <ozlmaltn8@gmail.com>
2024-12-27 16:41:18 +0300

    Home_update

--- resources/views/layouts/sections/home/_home.blade.php
+++ resources/views/layouts/sections/home/_home.blade.php
@@ -1,22 +1,142 @@
-<h2 class="text-center mt-5 mb-5">Home Sayfası</h2>
-
-
 <div class="container">
     <div class="row">
         <div class="col-md-4">
             Chatboot Kısmı
+            <div class="row">
+                <div class="col-md-12" style="background-color: red" width="100%" height="800 px">
+                    <form action="">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label"> Kullanıcı adınız:</label>
+                            <input type="email" class="form-control" id="exampleFormControlInput1"
+                                placeholder="Kullanıcı adınızı yazınız..">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlTextarea1" class="form-label">Merhaba, Nasıl yazdımcı
+                                olabilirim?</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                        <button type="button" class="btn btn-warning">Gönder</button>
+
+                    </form>
+
+                </div>
+            </div>
+
+        </div>
+        {{-- soruda diyo ya ne tür mekan değil de ne için randevu oluşturacaksınız diye
+        Romantik randevu
+        Aile Buluşması
+        İş Toplantısı
+        Kutlama
+        Sosyal Etkinlik (Eğlence mekanları-workshop mekanları gibi)
+        Öğrenci 
+        Özel bir randevu isteğim yok -- Öneri ? -- (Yapay zekaya yönlendir)
+ --}}
+
+
+        <div class="col-md-2 portfolio-jobs" id="deneme">
+
+            <a class="portfolio-job" target="_blank" href="ozelgun.blade.php">
+                <p class="portfolio-job__header text-center">Kutlama</p>
+
+                <p class="portfolio-job__description">
+                    <span class="portfolio-job__description-header text-center">Kutlama Konseptine uygun
+                        Restorantlar</span>
+                    <span class="delimiter"></span>
+                    <span class="portfolio-job__description-text text-center">Restaurant web-site</span>
+                </p>
+            </a>
+
+        </div>
+        <div class="col-md-2 portfolio-jobs ">
+
+
+
+            <a class="portfolio-job" target="_blank" href="ozelgun.blade.php">
+                <p class="portfolio-job__header text-center"> Aile Buluşması</p>
+
+                <p class="portfolio-job__description">
+                    <span class="portfolio-job__description-header text-center"> Aile Buluşması Konsept
+                        Restaurantları</span>
+                    <span class="delimiter"></span>
+                    <span class="portfolio-job__description-text text-center">Restaurant web-site</span>
+                </p>
+            </a>
+
+
+
         </div>
         <div class="col-md-2">
-            Restorant Menü 1
+            <div class="col-md-2 portfolio-jobs ">
+
+
+
+                <a class="portfolio-job" target="_blank" href="ozelgun.blade.php">
+                    <p class="portfolio-job__header text-center"> İş Toplantısı</p>
+
+                    <p class="portfolio-job__description">
+                        <span class="portfolio-job__description-header text-center"> İş Toplantısı Konsept
+                            Restaurantları</span>
+                        <span class="delimiter"></span>
+                        <span class="portfolio-job__description-text text-center">Restaurant web-site</span>
+                    </p>
+                </a>
+
+
+
+
+            </div>
+
         </div>
-        <div class="col-md-2">
-            Restorant Menü 2
+        <div class="row portfolio-jobs">
+            <div class="col-md-4 "></div>
+            <div class="col-md-2">
+
+
+                <a class="portfolio-job" target="_blank" href="ozelgun.blade.php">
+                    <p class="portfolio-job__header text-center"> Sosyal Etkinlik</p>
+
+                    <p class="portfolio-job__description">
+                        <span class="portfolio-job__description-header text-center"> Sosyal Etkinlik
+                            Restaurantları</span>
+                        <span class="delimiter"></span>
+                        <span class="portfolio-job__description-text text-center">Restaurant web-site</span>
+                    </p>
+                </a>
+
+
+
+
+            </div>
+            <div class="col-md-2">
+                <a class="portfolio-job" target="_blank" href="ozelgun.blade.php">
+                    <p class="portfolio-job__header text-center"> Sosyal Etkinlik</p>
+
+                    <p class="portfolio-job__description">
+                        <span class="portfolio-job__description-header text-center"> Sosyal Etkinlik
+                            Restaurantları</span>
+                        <span class="delimiter"></span>
+                        <span class="portfolio-job__description-text text-center">Restaurant web-site</span>
+                    </p>
+                </a>
+
+            </div>
+            <div class="col-md-2">
+                <a class="portfolio-job" target="_blank" href="ozelgun.blade.php">
+                    <p class="portfolio-job__header text-center"> Sosyal Etkinlik</p>
+
+                    <p class="portfolio-job__description">
+                        <span class="portfolio-job__description-header text-center"> Sosyal Etkinlik
+                            Restaurantları</span>
+                        <span class="delimiter"></span>
+                        <span class="portfolio-job__description-text text-center">Restaurant web-site</span>
+                    </p>
+                </a>
+
+            </div>
+
         </div>
-        <div class="col-md-2">
-            Restorant Menü 3
-        </div>
-        <div class="col-md-2">
-            Restorant Menü 4
-        </div>
+
+
+
     </div>
-</div>
