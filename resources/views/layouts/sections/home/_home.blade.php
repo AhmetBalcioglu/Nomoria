174fa19a0e38e80a83b0e93c15f9d0d6d48aff37
Ahmet <ahmetbalcioglu2002@hotmail.com>
2025-01-03 11:35:08 +0300

    Ana sayfa HTML kodları revize

--- resources/views/layouts/sections/home/_home.blade.php
+++ resources/views/layouts/sections/home/_home.blade.php
@@ -1,188 +1,170 @@
+<div class="container">
 
+    {{-- Kampanyalı Restoranlar Banner --}}
+    <div class="row">
+        <div class="col-md-12">
+            <h2 class="text-center mb-5 mt-5">Kampanyalı Restoranlar</h2>
+            <div class="slider">
 
-<div class="container mt-5">
+                <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false">
 
-    <div class="slider-wrapper slider-container">
-        <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="{{ asset('img/k1.png') }}" class="d-block w-100" alt="Kampanya 1">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="{{ asset('img/k1.png') }}" class="d-block w-100 img-fluid" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="{{ asset('img/k2.png') }}" class="d-block w-100 img-fluid" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="{{ asset('img/k3.png') }}" class="d-block w-100 img-fluid" alt="...">
+                        </div>
+                    </div>
+
+                    <button class="carousel-control-prev" type="button"
+                        data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+
+                    <button class="carousel-control-next" type="button"
+                        data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+
                 </div>
-                <div class="carousel-item">
-                    <img src="{{ asset('img/k2.png') }}" class="d-block w-100" alt="Kampanya 2">
+            </div>
+        </div>
+    </div>
 
+    {{-- En Çok Tercih Edilen --}}
+    <div class="row">
+        <h2 class="text-center mb-5 mt-5">En Çok Tercih Edilen</h2>
+        <div class="d-flex best">
 
+            <div class="col-md-2 border-2 border-dark p-2 mx-3 card card-body">
+                <p class="text-center"><b>Doğum Günü Rezervasyonları</b></p>
+                <img src="{{ asset('img/div_img.png') }}" width="50%" height="50%"
+                    class="d-block w-100 border border-dark border-1
+                     my-4" alt="">
+            </div>
 
+            <div class="col-md-2 border-2 border-dark p-2 mx-3 card card-body">
+                <p class="text-center"><b>İş Yemeği Rezervasyonları</b></p>
+                <img src="{{ asset('img/div_img.png') }}" width="50%" height="50%"
+                    class="d-block w-100 border border-dark border-1
+                     my-4" alt="">
+            </div>
 
+            <div class="col-md-2 border-2 border-dark p-2 mx-3 card card-body">
+                <p class="text-center"><b>Tek Kişilik Rezervasyonlar</b></p>
+                <img src="{{ asset('img/div_img.png') }}" width="50% " height="50%"
+                    class="d-block w-100 border border-dark border-1
+                     my-4" alt="">
+            </div>
 
+            <div class="col-md-2 border-2 border-dark p-2 mx-3 card card-body">
+                <p class="text-center"><b>Özel Gün Rezervasyonları</b></p>
+                <img src="{{ asset('img/div_img.png') }}" width="50%" height="50%"
+                    class="d-block w-100 border border-dark border-1
+                     my-4" alt="">
+            </div>
 
+            <div class="col-md-2 border-2 border-dark border-1
+             p-2 mx-3 card card-body">
+                <p class="text-center"><b>Kutlama Yemeği Rezervasyonları</b></p>
+                <img src="{{ asset('img/div_img.png') }}" width="50%" height="50%"
+                    class="d-block w-100 border border-dark border-1
+                     my-4" alt="">
+            </div>
+
+        </div>
+    </div>
+
+    {{-- Menü İçeriğine Göre Tercih Edilen --}}
+    <div class="row">
+        <div class="menubest">
+            <h2 class="text-center mb-5 mt-5">Menü İçeriğine Göre Tercih Edilen</h2>
+            <div class="d-flex best">
+
+                <div class="col-md-2 border-2 border-dark p-2 mx-3 card card-body">
+                    <p class="text-center"><b>Doğum Günü Rezervasyonları</b></p>
+                    <img src="{{ asset('img/div_img.png') }}" width="50%" height="50%"
+                        class="d-block w-100 border border-dark border-1
+                         my-4" alt="">
                 </div>
-                <div class="carousel-item">
-                    <img src="{{ asset('img/k3.png') }}" class="d-block w-100" alt="Kampanya 3">
 
-
-
-
-
-
+                <div class="col-md-2 border-2 border-dark p-2 mx-3 card card-body">
+                    <p class="text-center"><b>İş Yemeği Rezervasyonları</b></p>
+                    <img src="{{ asset('img/div_img.png') }}" width="50%" height="50%"
+                        class="d-block w-100 border border-dark border-1
+                         my-4" alt="">
                 </div>
 
+                <div class="col-md-2 border-2 border-dark p-2 mx-3 card card-body">
+                    <p class="text-center"><b>Tek Kişilik Rezervasyonlar</b></p>
+                    <img src="{{ asset('img/div_img.png') }}" width="50% " height="50%"
+                        class="d-block w-100 border border-dark border-1
+                         my-4" alt="">
+                </div>
+
+                <div class="col-md-2 border-2 border-dark p-2 mx-3 card card-body">
+                    <p class="text-center"><b>Özel Gün Rezervasyonları</b></p>
+                    <img src="{{ asset('img/div_img.png') }}" width="50%" height="50%"
+                        class="d-block w-100 border border-dark border-1
+                         my-4"
+                        alt="">
+                </div>
+
+                <div class="col-md-2 border-2 border-dark p-2 mx-3 card card-body">
+                    <p class="text-center"><b>Kutlama Yemeği Rezervasyonları</b></p>
+                    <img src="{{ asset('img/div_img.png') }}" width="50%" height="50%"
+                        class="d-block w-100 border border-dark border-1
+                         my-4"
+                        alt="">
+                </div>
 
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
         </div>
     </div>
 
+    {{-- Fiyata Göre Tercih Edilen --}}
+    <div class="row">
+        <div class="fiyat mb-5">
+            <h2 class="text-center mb-5 mt-5">Fiyata Göre Tercih Edilen</h2>
+            <div class="d-flex best">
 
+                <div class="col-md-2 border-2 border-dark p-2 mx-3 card card-body">
+                    <p class="text-center"><b>Doğum Günü Rezervasyonları</b></p>
+                    <img src="{{ asset('img/div_img.png') }}" width="50%" height="50%"
+                        class="d-block w-100 border border-dark border-2 my-4" alt="">
+                </div>
 
+                <div class="col-md-2 border-2 border-dark p-2 mx-3 card card-body">
+                    <p class="text-center"><b>İş Yemeği Rezervasyonları</b></p>
+                    <img src="{{ asset('img/div_img.png') }}" width="50%" height="50%"
+                        class="d-block w-100 border border-dark border-2 my-4" alt="">
+                </div>
 
+                <div class="col-md-2 border-2 border-dark p-2 mx-3 card card-body">
+                    <p class="text-center"><b>Tek Kişilik Rezervasyonlar</b></p>
+                    <img src="{{ asset('img/div_img.png') }}" width="50% " height="50%"
+                        class="d-block w-100 border border-dark border-2 my-4" alt="">
+                </div>
 
+                <div class="col-md-2 border-2 border-dark p-2 mx-3 card card-body">
+                    <p class="text-center"><b>Özel Gün Rezervasyonları</b></p>
+                    <img src="{{ asset('img/div_img.png') }}" width="50%" height="50%"
+                        class="d-block w-100 border border-dark border-2 my-4" alt="">
+                </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <div class="best card-container mt-3">
-        <div class="card">
-            <img src="{{ asset('img/div_img.png') }}" alt="Doğum Günü">
-            <p>Doğum Günü Rezervasyonları</p>
-        </div>
-        <div class="card">
-            <img src="{{ asset('img/div_img.png') }}" alt="İş Yemeği">
-            <p>İş Yemeği Rezervasyonları</p>
-        </div>
-        <div class="card">
-            <img src="{{ asset('img/div_img.png') }}" alt="Tek Kişilik">
-            <p>Tek Kişilik Rezervasyonlar</p>
-        </div>
-        <div class="card">
-            <img src="{{ asset('img/div_img.png') }}" alt="Özel Gün">
-            <p>Özel Gün Rezervasyonları</p>
-        </div>
-        <div class="card">
-            <img src="{{ asset('img/div_img.png') }}" alt="Kutlama Yemeği">
-            <p>Kutlama Yemeği Rezervasyonları</p>
-        </div>
-    </div>
-
-    <div class="best card-container mt-3">
-        <div class="card">
-            <img src="{{ asset('img/div_img.png') }}" alt="Doğum Günü">
-            <p>Doğum Günü Rezervasyonları</p>
-        </div>
-        <div class="card">
-            <img src="{{ asset('img/div_img.png') }}" alt="İş Yemeği">
-            <p>İş Yemeği Rezervasyonları</p>
-        </div>
-        <div class="card">
-            <img src="{{ asset('img/div_img.png') }}" alt="Tek Kişilik">
-            <p>Tek Kişilik Rezervasyonlar</p>
-        </div>
-        <div class="card">
-            <img src="{{ asset('img/div_img.png') }}" alt="Özel Gün">
-            <p>Özel Gün Rezervasyonları</p>
-        </div>
-        <div class="card">
-            <img src="{{ asset('img/div_img.png') }}" alt="Kutlama Yemeği">
-            <p>Kutlama Yemeği Rezervasyonları</p>
-        </div>
-    </div>
-
-    <div class="best card-container mt-3">
-        <div class="card">
-            <img src="{{ asset('img/div_img.png') }}" alt="Doğum Günü">
-            <p>Doğum Günü Rezervasyonları</p>
-        </div>
-        <div class="card">
-            <img src="{{ asset('img/div_img.png') }}" alt="İş Yemeği">
-            <p>İş Yemeği Rezervasyonları</p>
-        </div>
-        <div class="card">
-            <img src="{{ asset('img/div_img.png') }}" alt="Tek Kişilik">
-            <p>Tek Kişilik Rezervasyonlar</p>
-        </div>
-        <div class="card">
-            <img src="{{ asset('img/div_img.png') }}" alt="Özel Gün">
-            <p>Özel Gün Rezervasyonları</p>
-        </div>
-        <div class="card">
-            <img src="{{ asset('img/div_img.png') }}" alt="Kutlama Yemeği">
-            <p>Kutlama Yemeği Rezervasyonları</p>
-        </div>
-    </div>
-
-    <div class="best card-container mt-3">
-        <div class="card">
-            <img src="{{ asset('img/div_img.png') }}" alt="Doğum Günü">
-            <p>Doğum Günü Rezervasyonları</p>
-        </div>
-        <div class="card">
-            <img src="{{ asset('img/div_img.png') }}" alt="İş Yemeği">
-            <p>İş Yemeği Rezervasyonları</p>
-        </div>
-        <div class="card">
-            <img src="{{ asset('img/div_img.png') }}" alt="Tek Kişilik">
-            <p>Tek Kişilik Rezervasyonlar</p>
-        </div>
-        <div class="card">
-            <img src="{{ asset('img/div_img.png') }}" alt="Özel Gün">
-            <p>Özel Gün Rezervasyonları</p>
-        </div>
-        <div class="card">
-            <img src="{{ asset('img/div_img.png') }}" alt="Kutlama Yemeği">
-            <p>Kutlama Yemeği Rezervasyonları</p>
+                <div class="col-md-2 border-2 border-dark  p-2 mx-3 card card-body">
+                    <p class="text-center"><b>Kutlama Yemeği Rezervasyonları</b></p>
+                    <img src="{{ asset('img/div_img.png') }}" width="50%" height="50%"
+                        class="d-block w-100 border border-dark border-2 my-4" alt="">
+                </div>
+            </div>
         </div>
     </div>
 
 </div>
-
-
-<div id="popup" class="popup">
-    <button class="close-btn" onclick="closePopup()">×</button>
-    <div class="popup-content">
-        <img src="{{ asset('img/div_img.png') }}" alt="">
-        <h4>Hemen üye ol</h4>
-        <p>Beğendiğin restaurantları favorilerine ekle. </p>
-        <p>kampanylardan haberin olsun!</p>
-        <button class="btn btn-danger">ÜYE OL</button>
-    </div>
-</div>
-
-<script>
-
-    window.onload = function() {
-        setTimeout(function() {
-            document.getElementById("popup").style.display = "block";
-        }, 500);
-    }
-
-
-    function closePopup() {
-        document.getElementById("popup").style.display = "none";
-    }
-</script>
