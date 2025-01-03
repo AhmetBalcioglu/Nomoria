f69593b3bf55e78d6b6a61853cb32cd29c4aef50
Ali <aliartuc00@gmail.com>
2025-01-03 12:49:34 +0300

    Güncelleme ve Silme apileri eklendi

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use App\Models\Restaurant;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
+// Assuming the Restaurant model is in the App\Models namespace
+
 
 class RestaurantController extends Controller
 {
@@ -36,4 +41,77 @@ class RestaurantController extends Controller
 
         return view('layouts.sections.restaurants.index', compact('restaurants'));
     }
+
+    public function create(Request $request)
+    {
+        $image = time() . '.' . $request->file('image')->getClientOriginalExtension();
+
+
+        $destinationPath = public_path('images');
+        if (!File::exists($destinationPath)) {
+            File::makeDirectory($destinationPath, 0755, true, true);
+        }
+
+
+        $request->file('image')->move($destinationPath, $image);
+
+        $restaurant = new Restaurant();
+
+        $restaurant->guid = Str::uuid();
+        $restaurant->image = "/images/" . $image;
+        $restaurant->name = $request->name;
+        $restaurant->description = $request->description;
+        $restaurant->address = $request->address;
+        $restaurant->phone = $request->phone;
+        $restaurant->email = $request->email;
+
+        $restaurant->save();
+
+        return redirect()->route('home.home')->with('success', 'Restoran başarıyla oluşturuldu.');
+    }
+
+
+    public function delete(Request $request, $id){
+        $restaurant = Restaurant::find($id);
+
+        if ($restaurant) {
+            $restaurant->delete();
+            return redirect()->route('home.home')->with('success', 'Restoran başarıyla silindi.');
+        } else {
+            return redirect()->route('home.home')->with('error', 'Restoran bulunamadı.');
+        }
+    }
+
+    public function update(Request $request, $id){
+        $restaurant = Restaurant::find($id);
+
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'address' => 'nullable|string|max:255',
+            'phone' => 'nullable|integer|max:15',
+            'email' => 'nullable|email|max:255',
+        ]);
+
+        $restaurant->name = $request->input('name');
+        $restaurant->description = $request->input('description', $restaurant->description);
+        $restaurant->address = $request->input('address', $restaurant->address);
+        $restaurant->phone = $request->input('phone', $restaurant->phone);
+        $restaurant->email = $request->input('email', $restaurant->email);
+
+        if ($restaurant) {
+            $restaurant->update();
+            return redirect()->route('home.home')->with('success', 'Restoran başarıyla güncellendi.');
+        } else {
+            return redirect()->route('home.home')->with('error', 'Restoran güncellenemedi.');
+        }
+        
+ 
+    }
+
+    
+
 }
+
+
+
