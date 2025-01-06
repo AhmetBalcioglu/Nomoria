b617b0ac93691374b6cab5ddc89128b1b0f63312
Ahmet <ahmetbalcioglu2002@hotmail.com>
2025-01-06 13:37:43 +0300

    Header revize

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,7 +9,7 @@
     <!-- Navbar Section -->
     <nav class="navbar navbar-expand-lg navbar-light">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
@@ -37,7 +37,7 @@
             <button class="btn btn-secondary custom-button me-2">Favorilerim</button>
             <div class="dropdown">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person-fill mt-1 mx-1"
-                     viewBox="0 0 16 16" data-bs-toggle="dropdown" aria-expanded="false" width="24" height="24">
+                    viewBox="0 0 16 16" data-bs-toggle="dropdown" aria-expanded="false" width="24" height="24">
                     <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
                 </svg>
                 <ul class="dropdown-menu">
@@ -47,8 +47,8 @@
             </div>
         </div>
         <form class="d-flex mt-2">
-            <input id="searchBar" name="searchBar" class="form-control me-2 small-input" type="search"
-                   placeholder="Arama yapın" aria-label="Search">
+            <input id="searchBar" name="searchBar" class="form-control me-2" type="search" placeholder="Arama yapın"
+                aria-label="Search">
             <button class="btn btn-secondary btn-sm" type="submit">Ara</button>
         </form>
     </div>
