f22870e9fbf3003da7dd4f3eb86dcd9cac069e4c
pinar08 <pinar.polat1@gmail.com>
2025-01-08 18:17:00 +0300

    Create Menus.php

---
+++ app/Models/Menus.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Menus extends Model
+{
+    use HasFactory;
+
+    protected $table = 'menus';
+    protected $primaryKey = 'menuID';
+
+    protected $fillable = [
+        'restaurantID',
+        'name',
+        'description',
+        'price',
+    ];
+
+    // Restoran ile ilişki (Bir menü bir restorana aittir)
+    public function restaurant()
+    {
+        return $this->belongsTo(Restaurant::class, 'restaurantID', 'restaurantID');
+    }
+}
