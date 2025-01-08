7497c60d01130e9d7cdb17625ad91cb3a91239df
Bektas Ergin <bektas.ergin04@gmail.com>
2025-01-08 10:55:17 +0300

    fix

--- resources/views/addRestaurant/addRestaurant.blade.php
+++ resources/views/addRestaurant/addRestaurant.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 
-@section('page_title' , 'Restoran Ekleme - Nomoria')
+@section('page_title', 'Restoran Ekleme - Nomoria')
 
 @section('page_description', 'Nomoria Restoran Ekleme sayfasıdır.')
 
@@ -8,18 +8,17 @@
 
     @vite ('resources/css/addRestaurant/_addRestaurant.scss')
 
-    @section('content')
+@endsection
 
-        @include('layouts.sections.addRestaurant._addRestaurant')
+@section('content')
 
-    @endsection
+    @include('layouts.sections.addRestaurant._addRestaurant')
 
-
-
-         @vite('resources/js/addRestaurant/addRestaurant.js')
+@endsection
 
 
+@section('page_body_js')
 
+    @vite('resources/js/addRestaurant/addRestaurant.js')
 
-
-
+@endsection
