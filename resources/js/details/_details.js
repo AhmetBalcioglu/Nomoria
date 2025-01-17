89903730ab95ff47ee28588b870f5aa51745017c
Bektas Ergin <bektas.ergin04@gmail.com>
2025-01-17 20:11:13 +0300

    Kategori favorilemesi yapıldı scriptler düzeltildi
    kategori favorilemesi yapıldı, scriptler npme taşındı, favorileme kalpleri ayarlandı, favorileme hrefi verildi.

--- resources/js/details/_details.js
+++ resources/js/details/_details.js
@@ -35,8 +35,58 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  
+
 
 });
 
+$(document).ready(function () {
+  // Favori restoranlar için tıklama olayı
+  $('.bi-heart').on('click', function () {
+    const restaurantID = $(this).data('id'); // Tıklanan SVG'nin data-id değerini al
+    const svgElement = $(this); // Tıklanan SVG elementini seç
+
+    // AJAX isteği
+    $.ajax({
+      url: `/favorites/toggle/${restaurantID}`,
+      method: 'POST',
+      data: {
+        _token: $('meta[name="csrf-token"]').attr('content') // Dinamik CSRF token
+      },
+      success: function (response) {
+        if (response.success) {
+          if (response.added) {
+            svgElement.addClass('text-danger');
+            Swal.fire({
+              icon: 'success',
+              title: 'Favorilerinize eklendi.',
+            }).then(function () {
+              location.reload();
+            });
+          } else {
+            svgElement.removeClass('text-danger');
+            Swal.fire({
+              icon: 'success',
+              title: 'Favorilerinizden kaldırıldı.',
+            }).then(function () {
+              location.reload();
+            });
+          }
+        } else {
+          Swal.fire({
+            icon: 'error',
+            title: 'Bir hata oluştu.',
+            text: response.message,
+          });
+        }
+      },
+      error: function (xhr) {
+        Swal.fire({
+          icon: 'error',
+          title: 'AJAX isteği başarısız.',
+          text: xhr.statusText,
+        });
+      }
+    });
+  });
+});
 
