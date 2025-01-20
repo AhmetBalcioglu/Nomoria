fe7c7bd0aaf13df4383262337dbe7cff1dfd4c0f
Ali <aliartuc00@gmail.com>
2025-01-21 01:42:31 +0300

    Merge branch 'main' of https://github.com/AhmetBalcioglu/Nomoria

--- resources/views/layouts/sections/contact/_contact.blade.php
+++ resources/views/layouts/sections/contact/_contact.blade.php
@@ -1,21 +1,37 @@
 <div class="contact-container mt-5 mb-5">
     <div class="concant-info " >
-        <h1>Memnuniyet Anketi</h1> <br>
+        <h1>Nomoria nasıl kullanılır ?</h1> <br>
 
         
         <div class="social-icons">
            <img src="{{ asset('img/form_image.png') }}" alt="" width="250px" ">
         </div>
     </div>
-    <div class="contact-form">
-        <!--<h1 style="text-align: center;">Bize Ulaşın</h1>-->
-        <form action="{{ route('contact.send') }}" method="POST">
-            @csrf
-            <input type="text" name="name" placeholder="Adınız" required>
-            <input type="text" name="surname" placeholder="Soyadınız" required>
-            <input type="email" name="email" placeholder="E-mail Adresiniz" required>
-            <textarea name="message" placeholder="Mesajınız" required></textarea>
-            <button type="submit" class="btn btn-primary">Gönder</button>
-        </form>
-    </div>
+  
 </div>
+
+
+
+
+
+<div class="contact-container mt-5 mb-5">
+    <div class="concant-info ">
+        <h1>Memnuniyet Anketi</h1> <br>
+
+
+        <div class="social-icons">
+            <img src="{{ asset('img/form_image.png') }}" alt="" width="250px" ">
+        </div>
+    </div>
+    <div class=" contact-form">
+            <!--<h1 style="text-align: center;">Bize Ulaşın</h1>-->
+            <form action="{{ route('contact.send') }}" method="POST">
+                @csrf
+                <input type="text" name="name" placeholder="Adınız" required>
+                <input type="text" name="surname" placeholder="Soyadınız" required>
+                <input type="email" name="email" placeholder="E-mail Adresiniz" required>
+                <textarea name="message" placeholder="Mesajınız" required></textarea>
+                <button type="submit" class="btn btn-primary">Gönder</button>
+            </form>
+        </div>
+    </div>
