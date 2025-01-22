3da080b92d575657308b69a18bfc7d0a769ec85d
Bektas Ergin <bektas.ergin04@gmail.com>
2025-01-22 11:15:23 +0300

    rezervasyon oluşturunca mail gönderiyor

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -7,12 +7,15 @@ use Illuminate\Http\Request;
 use App\Http\Requests\ReservationCreateRequest;
 use App\Models\Restaurant;
 use Carbon\Carbon;
+use App\Http\Controllers\Mail\ReservationCreatedMail;
+use Illuminate\Support\Facades\Mail;
+
 
 class ReservationController extends Controller
 {
     public function index()
     {
-        $reservations = Reservation::with('restaurant.cities', 'restaurant.districts')->get()->toArray();
+        $reservations = Reservation::with('restaurant.name', 'restaurant.cities', 'restaurant.districts')->get()->toArray();
         return view("reservations.reservations", compact('reservations'));
     }
     public function makeReservation()
@@ -23,32 +26,36 @@ class ReservationController extends Controller
     {
         $reservation = new Reservation();
 
-        $restaurant = Restaurant::find($request->input('restaurantID'));
+        $restaurant = Restaurant::where('restaurantID', $request->input('restaurantID'))->first();
+        if (!$restaurant) {
+            return response()->json(['success' => false, 'message' => 'Restoran bulunamadı.']);
+        }
+
         $restaurantCapacity = $restaurant->capacity;
 
         if ($request->input('guestCount') > $restaurantCapacity) {
-            return response()->json(['success' => false, 'message' => 'Restoran kapasitesi dolmuştur.',]);
+            return response()->json(['success' => false, 'message' => 'Restoran kapasitesi dolmuştur.']);
         }
 
         if ($request->date < Carbon::now()->format('Y-m-d')) {
             return response()->json(['success' => false, 'message' => 'Lütfen geçerli bir tarih giriniz'], 400);
         }
 
-
         $reservation->restaurantID = $request->input('restaurantID');
-        $reservation->userID = $request->input('userID');
+        $reservation->userID = session('userID');
         $reservation->reservation_time = $request->input('date');
         $reservation->guest_count = $request->input('guestCount');
         $reservation->created_at = Carbon::now();
 
         try {
+            $userName = session('name');
+            $userEmail = session('email');
             $reservation->save();
+            Mail::to($userEmail)->send(new ReservationCreatedMail($reservation, $userName));
 
-            //TODO: Rezervasyon yaptıktan sonra maile mesaj gönder
-
-            return response()->json(['success' => true, 'message' => 'Rezervasyon başarıyla oluşturuldu.',]);
+            return response()->json(['success' => true, 'message' => 'Rezervasyon başarıyla oluşturuldu.']);
         } catch (\Throwable $errorMessage) {
-            return response()->json(['success' => false, 'message' => $errorMessage->getMessage(),]);
+            return response()->json(['success' => false, 'message' => $errorMessage->getMessage()]);
         }
     }
 }
