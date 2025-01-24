42be616b451d06702c9db19e899c0edf590bc1b8
Ozlmaltn <ozlmaltn8@gmail.com>
2025-01-24 11:34:17 +0300

    profil tasarımı revize

--- resources/views/layouts/sections/profile/_profile.blade.php
+++ resources/views/layouts/sections/profile/_profile.blade.php
@@ -1,13 +1,13 @@
 
-<div class="container mt-5" style="margin-bottom: 30rem">
+<div class="container mt-5" style="margin-bottom: 30rem" id="profiles">
     <div class="row">
         <div class="col-md-12">
             <h1>Profil Bilgileri</h1>
-            <p><b>Ad:</b> {{ session()->get('name') }}</p>
-            <p><b>Soyad:</b> {{ session()->get('surname') }}</p>
-            <p><b>Cinsiyet:</b> {{ session()->get('gender') }}</p>
-            <p><b>Rol:</b> {{ session()->get('role')}}</p>
-            <p><b>E-posta:</b> {{ session()->get('email') }}</p>
+            <p class="information"><b>Ad:</b> {{ session()->get('name') }}</p>
+            <p class="information"><b>Soyad:</b> {{ session()->get('surname') }}</p>
+            <p class="information"><b>Cinsiyet:</b> {{ session()->get('gender') }}</p>
+            <p class="information"><b>Rol:</b> {{ session()->get('role')}}</p>
+            <p class="information"><b>E-posta:</b> {{ session()->get('email') }}</p>
         </div>
     </div>
 </div>
