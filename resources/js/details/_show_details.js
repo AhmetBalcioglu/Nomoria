dc36255feeb722a71c0c20f9e72164301f0c4fe2
Bektas Ergin <bektas.ergin04@gmail.com>
2025-01-25 16:51:02 +0300

    restoran detay revize edildi

--- resources/js/details/_show_details.js
+++ resources/js/details/_show_details.js
@@ -1,28 +1,28 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     const labels = document.querySelectorAll(".rating label");
+document.addEventListener("DOMContentLoaded", function () {
+    const labels = document.querySelectorAll(".rating label");
 
-//     labels.forEach(label => {
-//         label.addEventListener("click", function () {
-//             const rating = parseInt(this.getAttribute("data-rating"));
-//             highlightStars(rating);
-//         });
-//     });
+    labels.forEach(label => {
+        label.addEventListener("click", function () {
+            const rating = parseInt(this.getAttribute("data-rating"));
+            highlightStars(rating);
+        });
+    });
 
-//     function highlightStars(rating) {
-//         labels.forEach((label, index) => {
-//             label.style.color = (index < rating) ? "gold" : "black";
-//         });
-//     }
-// });
+    function highlightStars(rating) {
+        labels.forEach((label, index) => {
+            label.style.color = (index < rating) ? "gold" : "black";
+        });
+    }
+});
 
-// const updateBtns = document.querySelectorAll('.comment-update-btn');
-// const updateForms = document.querySelectorAll('.comment-update-form');
+const updateBtns = document.querySelectorAll('.comment-update-btn');
+const updateForms = document.querySelectorAll('.comment-update-form');
 
-// updateBtns.forEach((btn, index) => {
-//     btn.addEventListener('click', () => {
-//         updateForms[index].classList.toggle('d-none');
-//     });
-// });
+updateBtns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        updateForms[index].classList.toggle('d-none');
+    });
+});
 
  $(document).ready(function () {
      $(".openMenuModal").on("click", function () {
