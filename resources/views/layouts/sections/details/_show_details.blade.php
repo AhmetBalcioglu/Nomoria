e07120dbd0b74ac84da2a38741dbcf4713492632
Ali <aliartuc00@gmail.com>
2025-01-26 23:49:01 +0300

    show details dark-mode revize

--- resources/views/layouts/sections/details/_show_details.blade.php
+++ resources/views/layouts/sections/details/_show_details.blade.php
@@ -19,7 +19,7 @@
 
 
 <!-- Yönlendirme sekmeleri (Hakkında, Galeri, Menü, Yorumlar) -->
-<nav class="navbar navbar-expand-lg" id="showDetails">
+<nav id="showDetails" class="navbar navbar-expand-lg navbar-light bg-light" >
     <div class="container">
         <ul class="navbar-nav mx-auto">
             <li class="nav-item">
